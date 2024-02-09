310c9521d0139e0cc5b0d4c7fdfd5d2255e1e226
Mona <nairamonteiro3@gmail.com>
2024-02-09 13:06:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
-//variavéis
-    const passwordBox = document.getElementById("password");
-    const length = 12;
-    const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    const numbers = "0123456789";
-    const symbols = "!@#$%^&*()-_=+[{]}\\|;:\,<.>/?";
-    const allChars = upperCase + lowerCase + numbers + symbols;
+        const senhaCaixa = document.getElementById("password");
+        const comprimento = 12;
+        const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+        const numeros = "0123456789";
+        const simbolos = "!@#$%^&*()-_=+[{]}\\|;:,<.>/?";
+        const todos = upperCase + lowerCase + numeros + simbolos;
 
-//funções
-//criação de senha
-    function createPassword(){
-        let password = "";
-                for(let i = 0; i < length; i++){
-                    let randomNumber = Math.floor(Math.random() * 4);
-                    switch(randomNumber){
-                        case 0:
-                            password += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
-                            break;
-                        case 1:
-                            password += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
-                            break;
-                        case 2:
-                            password += numbers.charAt(Math.floor(Math.random() * numbers.length));
-                            break;
-                        case 3:
-                            password += symbols.charAt(Math.floor(Math.random() * symbols.length));
-                            break;
-                    }
+        function criarPassword() {
+            let password = "";
+            for (let i = 0; i < comprimento; i++) {
+                let aleatorio = Math.floor(Math.random() * 4);
+                switch (aleatorio) {
+                    case 0:
+                        password += upperCase.charAt(Math.floor(Math.random() * upperCase.length));
+                        break;
+                    case 1:
+                        password += lowerCase.charAt(Math.floor(Math.random() * lowerCase.length));
+                        break;
+                    case 2:
+                        password += numeros.charAt(Math.floor(Math.random() * numeros.length));
+                        break;
+                    case 3:
+                        password += simbolos.charAt(Math.floor(Math.random() * simbolos.length));
+                        break;
                 }
-                passwordBox.value = password;
+            }
+            senhaCaixa.value = password;
+        }
 
-                while (length > password.length){
-                    passwordBox.value += allChars.charAt(Math.floor(Math.random() * allChars.length));
-                }
-    }
-
-//copiar senha
-    function copyPassword(){
-        passwordBox.select();
-        navigator.clipboard.writeText(passwordBox.value);
+    function copiarPassword(){
+        senhaCaixa.select();
+        navigator.clipboard.writeText(senhaCaixa.value);
+        alert("Senha copiada para a área de transferência!");
     }
